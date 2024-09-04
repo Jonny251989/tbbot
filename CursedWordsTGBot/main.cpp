43603f23cbed1ba9e279c8935a45a96156d173db
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-09-04 11:33:41 +0300

    correct cmakelist

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -13,10 +13,39 @@
 
 int main(int argc, char **argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
+
+     TgBot::Bot bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
+        bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+            bot.getApi().sendMessage(message->chat->id, "Hi!");
+        });
+        bot.getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
+            printf("User wrote %s\n", message->text.c_str());
+
+            
+            if (StringTools::startsWith(message->text, "/start")) {
+                return;
+            }
+
+            bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+        
+        try {
+            printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+
+            TgBot::TgLongPoll longPoll(bot);
+
+            while (true) {
+                printf("Long poll started\n");
+                longPoll.start();
+            }
+        } catch (TgBot::TgException& e) {
+            printf("error: %s\n", e.what());
+        }
+
+//   ::testing::InitGoogleTest(&argc, argv);
+//   ::testing::InitGoogleMock(&argc, argv);
   
-  return RUN_ALL_TESTS();
+//   return RUN_ALL_TESTS();
 }
 
 
